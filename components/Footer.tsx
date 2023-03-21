e2ea4a5fc30009b9478d556961290477414ba6b4
@猪锈氢 <1667834841@qq.com>
2023-03-21 10:57:43 +0800

    footer添加弹出框

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,15 +1,18 @@
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Footer() {
   return (
     <footer className="text-center h-16 sm:h-20 w-full sm:pt-2 pt-4 border-t mt-5 flex sm:flex-row flex-col justify-center items-center px-3 space-y-3 sm:mb-0 mb-3 bottom-0">
+      
       <div>
+      <ContactLink />
         Powered by{" "}
         <a
           href="https://openai.com/"
           target="_blank"
           rel="noreferrer"
-          className="font-bold hover:underline transition underline-offset-2"
+          className=" hover:underline transition underline-offset-2"
         >
           OpenAI{" "}
         </a>
@@ -18,25 +21,80 @@ export default function Footer() {
           href="https://vercel.com/"
           target="_blank"
           rel="noreferrer"
-          className="font-bold hover:underline transition underline-offset-2"
+          className=" hover:underline transition underline-offset-2"
         >
           Vercel Edge Functions.
         </a>
+        
       </div>
-      {/* <div className="flex space-x-4 pb-4 sm:pb-0">
-        <Link
-          href="https://github.com/guaguaguaxia/weekly_report"
-          className="group"
-          aria-label="TaxPal on GitHub"
-        >
-          <svg
-            aria-hidden="true"
-            className="h-6 w-6 fill-slate-500 group-hover:fill-slate-700"
-          >
-            <path d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0 1 12 6.844a9.59 9.59 0 0 1 2.504.337c1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.02 10.02 0 0 0 22 12.017C22 6.484 17.522 2 12 2Z" />
-          </svg>
-        </Link>
-      </div> */}
+
     </footer>
   );
 }
+
+const ContactLink = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <>
+      <a
+      target="_blank"
+          rel="noreferrer"
+        className="font-bold hover:cursor-pointer hover:outline-black transition underline-offset-2 block"
+        onClick={() => setShowModal(true)}
+      >
+        欢迎进交流反馈群，解锁更多prompt玩法
+      </a>
+      {showModal ? (
+        <div className="fixed z-10 inset-0">
+          <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <div className="fixed inset-0 transition-opacity min-h-screen" aria-hidden="true">
+              <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+            </div>
+            <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+            <div
+              className="z-99 inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+              role="dialog"
+              aria-modal="true"
+              aria-labelledby="modal-headline"
+            >
+              <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                <div className="sm:flex sm:items-center flex-col">
+
+                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                  <h3 className="text-2xl leading-6 font-bold text-black-900 flex justify-center items-center" id="modal-headline">
+                    交流群链接，讨论更多玩法
+                    <button
+                      type="button"
+                      className="inline-flex justify-center  border border-transparent  px-2 py-2  text-base font-medium text-black  focus:outline-none  focus:ring-black-500 sm:ml-3 sm:w-auto sm:text-sm"                        onClick={() => setShowModal(false)}
+                      style={{position: "absolute", top: "0", right: "0"}}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth={2}
+                          d="M6 18L18 6M6 6l12 12"
+                        />
+                      </svg>
+                    </button>
+                  </h3>
+                    
+                  <img src="https://img.ggball.top/picGo/wwj.png" alt="交流群链接" style={{display: "block", margin: "auto"}}/>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      ) : null}
+    </>
+  );
+};
