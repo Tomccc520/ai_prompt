b16f9b3d72809d6e5eca429d7fc651e93ea1153a
@猪锈氢 <1667834841@qq.com>
2023-03-26 12:51:34 +0800

    关闭提前预览

--- components/Card.tsx
+++ components/Card.tsx
@@ -9,7 +9,7 @@ export default function Card({index}) {
     
     return (
       
-        <Link prefetch  className="rounded-lg bg-white shadow-xl shadow-black/5 ring-1 ring-slate-700/10" href={{ pathname: "/custom", query: { index: index } }}>
+        <Link  className="rounded-lg bg-white shadow-xl shadow-black/5 ring-1 ring-slate-700/10" href={{ pathname: "/custom", query: { index: index } }}>
         <div className="flex flex-col p-4">
           <div className="flex items-center gap-2">
             <span className="text-lg font-semibold text-black">{t("title")}</span>
